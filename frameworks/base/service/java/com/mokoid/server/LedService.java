f87c319f740b8d722209aee76ba35fe6e8326f49
pgalxx <xiaian168@qq.com>
2012-10-27 13:24:35 +0800

    Update frameworks/base/service/java/com/mokoid/server/LedService.java

--- frameworks/base/service/java/com/mokoid/server/LedService.java
+++ frameworks/base/service/java/com/mokoid/server/LedService.java
@@ -49,6 +49,7 @@ public final class LedService extends ILedService.Stub {
     public boolean setOn(int led) {
         Log.i("MokoidPlatform", "LED On");
 	return _set_on(led);
+        //return _set_on(5.1);
     }
 
     public boolean setOff(int led) {
@@ -57,6 +58,6 @@ public final class LedService extends ILedService.Stub {
     }
 
     private static native boolean _init();
-    private static native boolean _set_on(int led);
+    private static native boolean _set_on(int led);// private static native boolean _set_on(int led,float timeout);
     private static native boolean _set_off(int led);
 }
